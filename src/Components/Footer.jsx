9f66a6f94608e9af340a7eb529e8487a5a3e2891
Abhishek-1839 <abhishektomarsingh7@gmail.com>
2025-02-22 18:00:27 +0530

    first commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import logo from "../assets/logo1.png";
+import linkedin from "../assets/linkedin.svg";
+import instagram from "../assets/instagram.svg";
+// import { FaInstagram, FaLinkedin } from "react-icons/fa";
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <div className={styles.footeroneContainer}>
+            <img src={logo} alt="JustLiv Logo" className={styles.logo} />
+            <p className={styles.social}>
+                {/* <FaInstagram className={styles.icon} />  */}
+                <img src={instagram}
+                    alt="LinkedIn"
+                    className={styles.linkedin}
+                /> Connect with us on Instagram ♡
+            </p>
+            </div>
+
+            <div className={styles.footertwoContainer}>
+            <div className={styles.links}>
+                <a href="/privacy-policy" className={styles.privac}>Privacy policy</a>
+                <br />
+                <br/>
+                <a href="/accessibility" className={styles.accessb}>Accessibility Statement</a>
+            </div>
+            <div className={styles.socialIcons}>
+                <a href="https://www.linkedin.com/company/justliv" target="_blank" rel="noreferrer">
+                <img src={linkedin}
+                    alt="LinkedIn"
+                    className={styles.linkedin}
+                />
+                </a>
+            </div>
+            <p className={styles.copyright}>© 2024 by JustLiv</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/Components/Work.jsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import DownArrowImage from '../assets/downArrow.png'
+import TwistedArrowImage from '../assets/twistedArrow.png'
+import SkyImage from "../assets/sky.png";
+import styles from "./Work.module.css";
+
+const Work = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 430);
+  
+    useEffect(() => {
+      const handleResize = () => {
+        setIsMobile(window.innerWidth <= 430);
+      };
+  
+      window.addEventListener('resize', handleResize);
+      return () => window.removeEventListener('resize', handleResize);
+    }, []);
+  return (
+    <div className={styles.WorkSection}>
+      <div>
+      <img src={SkyImage} alt="Hero" className={styles.SkyImage} />
+      <div className={styles.stepsoverlay}>
+        <h2 className={styles.workheading}>How it works</h2>
+       
+        <div className={styles.step}>
+          <div className={styles.econ}>
+            <SearchOutlinedIcon className={styles.icon} />
+          </div>
+          <div className={styles.details}>
+            <h3>Browse</h3>
+            {isMobile && <p className={styles.mtext}>Discover activities from our partner <br/> booking providers.</p>}
+            <p className={styles.ntext}>Discover activities from our partner booking providers.</p>
+          </div>
+        </div>
+        <img src={DownArrowImage} className={styles.down} />
+        <div className={styles.step}>
+          <div className={styles.econ}>
+            <CalendarTodayOutlinedIcon className={styles.icon} />
+          </div>
+
+          <div className={styles.details}>
+            
+            <h3>Build</h3>
+            {isMobile && <p className={styles.mtext}>Share your child’s interests,<br /> schedule, and preferences. Watch <br /> 
+            as JustLiv creates the perfect <br /> calendar filled with activities.</p>}
+            <p className={styles.ntext}>Share your child’s interests, schedule, and preferences.</p>
+          </div>
+        </div>
+        <img src={TwistedArrowImage}  className={styles.twisted} />
+        <div className={styles.stepp}>
+          <div className={styles.econ}>
+            {" "}
+            <FavoriteBorderIcon className={styles.icon} />
+          </div>
+          <div className={styles.details}>
+            {" "}
+                  
+            <h3>Book</h3>
+            {isMobile && <p className={styles.mtext}>Finalize your selections with one click <br /> and enjoy peace of mind.</p>}
+            <p className={styles.ntext}>
+              Finalize your selections with one click and enjoy peace of mind.
+            </p>
+          </div>
+        </div>
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
